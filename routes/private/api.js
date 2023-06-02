a9f179db7cd7c36de00cb75e60321e6c211aa6f8
Mariam-mimi7 <mariamokby@gmail.com>
2023-06-02 22:06:35 +0200

    Initial commit

---
+++ routes/private/api.js
@@ -0,0 +1,170 @@
+const { isEmpty, get } = require("lodash");
+const { v4 } = require("uuid");
+const db = require("../../connectors/db");
+const roles = require("../../constants/roles");
+const {getSessionToken}=require('../../utils/session')
+const getUser = async function (req) {
+  const sessionToken = getSessionToken(req);
+  if (!sessionToken) {
+    return res.status(301).redirect("/");
+  }
+  console.log("hi",sessionToken);
+  const user = await db
+    .select("*")
+    .from("se_project.sessions")
+    .where("token", sessionToken)
+    .innerJoin(
+      "se_project.users",
+      "se_project.sessions.userid",
+      "se_project.users.id"
+    )
+    .innerJoin(
+      "se_project.roles",
+      "se_project.users.roleid",
+      "se_project.roles.id"
+    )
+   .first();
+
+  console.log("user =>", user);
+  user.isNormal = user.roleid === roles.user;
+  user.isAdmin = user.roleid === roles.admin;
+  user.isSenior = user.roleid === roles.senior;
+  console.log("user =>", user)
+  return user;
+};
+
+module.exports = function (app) {
+  // example
+  app.get("/users", async function (req, res) {
+    try {
+       const user = await getUser(req);
+      const users = await db.select('*').from("se_project.users")
+        
+      return res.status(200).json(users);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not get users");
+    }
+   
+  });
+ //Reset Password
+ app.put("/api/v1/password/reset" , async function(req , res){
+  const user = await getUser(req);
+  const newpassword = req.body.newpassword;
+  if(!newpassword){
+     return res.status(400).send("Required!");
+  }else{
+      db.from("se_project.users")
+      .where("email" , user.email)
+      .update({password:newpassword}).then(function(rowsUpdated){
+        res.status(200).json({message: 'password updated'})});
+    }
+ });
+
+ app.post("/api/v1/payment/ticket" , async function(req,res){
+   const {creditCardNumber , holderName , payedAmount , origin , destination , tripDate } = req.body;
+   const userId = await getUser(req);
+   const userhasSub = await db.select('userid').from("subsription");
+   if(userhasSub.userid == userId.id){
+     return res.status(400).send("Failed.You have a subscription");
+   }else{
+   
+    ticket_data = {
+     origin: req.body.origin,
+     destination:req.body.destination,
+      userid:userId.userid,
+      tripdate:req.body.tripDate
+    };
+     const ticket = await db("tickets").where("origin" , ' = ' , origin).where("destination" ,'=', destination)
+     .where("tripddate" , '=', tripDate)
+     .insert(ticket_data).returning("*")
+    
+    if(ticket == true){ 
+      tran_data={
+        amount:req.body.payedAmount,
+        userid:userId.userid,
+        purchasediid:ticket.id
+      };
+      const tran = await db("se_project.transactions")
+      .where("amount" , '=' , payedAmount)
+      .insert(tran_data)
+   }if (ticket){
+     return res.status(200).send("Ticket purchased");
+   }else{
+    return res.status(400).send("Failed.Ticket not purchased");
+   }
+  }
+ });
+ app.post("/api/v1/payment/subscription" , async function(req,res){
+   const { creditCardNumber , holderName , payedAmount , subType , zoneId } = req.body;
+   const user = await getUser(req);
+     if(subType == "annual"){
+         nooftickets =100;
+      }else if (subType == "quarterly"){
+           nooftickets = 50;
+      } else if (subType == "monthly"){
+           nooftickets = 10;
+      }
+      
+  const subscription_data = {
+   subtype:req.body.subType,
+   zoneid:req.body.zoneId,
+   userid:user.userid,
+   nooftickets
+ }     
+   const insertsubs =await db("se_project.subsription")
+   .where("subtype" , '=' , subType)
+    .where("zoneid" , '=' , zoneId)
+   .where("nooftickets" , '=' , nooftickets)
+   .insert(subscription_data).returning("*");
+  if(insertsubs){
+    if(nooftickets == 100){
+      return res.status(200).send("Subscription paid now you got 100 tickets");
+    
+   }else if (nooftickets == 50){
+        return res.status(200).send("Subscription paid now you got 50 tickets");
+        
+   } else if (nooftickets == 10){
+       return res.status(200).send("Subscription paid now you got 10 tickets");
+   } 
+  }else{
+  return res.status(400).send("Could not pay subscription");  
+  }  
+  const transaction_data = {
+    amount:req.body.payedAmount,
+    userid:user.userid,
+    purchasediid: insertsubs.id
+  }
+  const tran = await db("se_project.transactions")
+   .where("amount" , '=' , payedAmount)
+   .insert(transaction_data).returning('*')
+
+  });
+
+ app.post("/api/v1/tickets/purchase/subscription" , async function(req , res){
+   const userID = await getUser(req);
+   const {subId , origin , destination , tripDate} = req.body;
+   const userhasSub = await db.select('userid').from("subsription");
+        if(userhasSub.userid == userID.id){
+          const ticket_data = {
+            userid:userID.id,
+            subid:req.body.subId,
+            origin:req.body.origin,
+            destination:req.body.destination,
+            tripdate:req.body.tripDate
+          };
+           const insertticket = await db("se_project.tickets")
+           .where("subid" , '=' , subId)
+           .where("origin" , '=' , origin)
+           .where("destination" , '=' , destination)
+           .where("tripdate" , '=' , tripDate)
+           .insert(ticket_data);
+
+             if(insertticket){
+          return res.status(200).send("Ticket payed through subscription successfully!");
+             }
+        }else{
+          return res.status(400).send("Failed.You have no subscription!");
+        }
+ });
+};
---
+++ routes/public/api.js
@@ -0,0 +1,123 @@
+const { isEmpty } = require("lodash");
+const { v4 } = require("uuid");
+const db = require("../../connectors/db");
+const roles = require("../../constants/roles");
+module.exports = function (app) {
+//Get zones
+app.get("/api/v1/zones" , async function(req,res){
+  try{
+      const zones = await db.select("*")
+      .from("zones");
+      res.status(200).send(zones);
+      }catch(e){
+      res.status(500).send(e);
+  }
+});
+//Get  Price
+  app.get("/api/v1/tickets/price/:originId/:destinationId", async function(req, res) {    
+    const { originId, destinationId } = req.params;  // using req.params instead of req.query
+    const from = await db("se_project.routes")
+      .select(["fromstationid"])
+      .where("fromstationid", "=", originId);
+    const to = await db("se_project.routes")
+      .select(["tostationid"])
+      .where("tostationid", "=", destinationId);
+  
+    for (let i = 0; i < from.length; i++) {
+      for (let j = 0; j < to.length; j++) {
+        if (from[i].fromstationid >= 1 && to[j].tostationid <=9) {
+          res.status(200).send("From Stations 1 through 9 = Ticket price is 5LE");
+        } else if (from[i].fromstationid >=10 && to[j].tostationid <= 16) {
+          res.status(200).send("From Stations 10 through  16 = Ticket price is 7LE");
+        } else if (from[i].fromstationid >= 16 && to[j].tostationid > 16) {
+          res.status(200).send("From Station 16 and more = Ticket price is 10LE");
+        }
+      }
+    }
+  });
+//create user 
+app.post("/api/v1/user/login", async function (req, res) {
+    // get users credentials from the JSON body
+    const { email, password  , roleid} = req.body;
+    if (!email) {
+      // If the email is not present, return an HTTP unauthorized code
+      return res.status(400).send("email is required");
+    }
+    if (!password) {
+      // If the password is not present, return an HTTP unauthorized code
+      return res.status(400).send("Password is required");
+    }
+    
+
+
+    // validate the provided password against the password in the database
+    // if invalid, send an unauthorized code
+      const user = await db
+      .select("*")
+      .from("se_project.users")
+      .where("email", email)
+      .first();
+    
+ 
+    if (isEmpty(user)) {
+      return res.status(400).send("user does not exist");
+    }
+
+    if (user.password !== password) {
+      return res.status(401).send("Password does not match");
+    }
+    
+
+    // set the expiry time as 15 minutes after the current time
+    const token = v4();
+    const currentDateTime = new Date();
+    const expiresat = new Date(+currentDateTime + 900000); // expire in 15 minutes
+
+    // create a session containing information about the user and expiry time
+    const session = {
+      userid: user.id,
+      token,
+      expiresat,
+    };
+    try {
+      await db("se_project.sessions").insert(session);
+      // In the response, set a cookie on the client with the name "session_cookie"
+      // and the value as the UUID we generated. We also set the expiration time.
+      return res
+        .cookie("session_token", token, { expires: expiresat })
+        .status(200)
+        .send("login successful");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not register user");
+    }
+  });
+  app.post("/api/v1/user", async function (req, res) {
+
+    // Check if user already exists in the system
+    const userExists = await db
+      .select("*")
+      .from("se_project.users")
+      .where("email", req.body.email);
+    if (!isEmpty(userExists)) {
+      return res.status(400).send("user exists");
+    }
+
+    const newUser = {
+      firstname: req.body.firstName,
+      lastname: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      roleid: roles.user,
+    };
+    try {
+      const user = await db("se_project.users").insert(newUser).returning("*");
+
+      return res.status(200).json(user );
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not register user");
+    }
+  });
+
+}
